621b0988e19d359146a44b7a4a8fb1d4198057dd
Michael Weber <michaelwtv@gmail.com>
2017-12-17 13:16:47 +0100

    Fixed bug in xmlUtils.py

--- Raspberry Pi files/Untested code/xmlUtils.py
+++ Raspberry Pi files/Untested code/xmlUtils.py
@@ -4,7 +4,7 @@ def parseXml(data):
     return EL.fromstring(qrcodetext)
     
 def checkSuccess(xml):
-    return xml.getroot().text != 'success'
+    return xml.getroot().text == 'success'
 
 def setPresented(xml, presented):
     presentedTag = EL.SubElement(xml.getroot(), 'presented')
