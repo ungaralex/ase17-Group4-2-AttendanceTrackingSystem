fd16d56e6648be44ce4b163d80d5eb4d31d81635
Michael Weber <michaelwtv@gmail.com>
2018-01-11 17:13:08 +0100

    Added basic authentication. Student services: auth. through google account Tutor services: auth. through hard coded credentials

--- src/main/java/com/ase/group42/webinterface/AttendanceTrackingRESTApplication.java
+++ src/main/java/com/ase/group42/webinterface/AttendanceTrackingRESTApplication.java
@@ -2,7 +2,12 @@ package com.ase.group42.webinterface;
 
 import org.restlet.Application;
 import org.restlet.Restlet;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.ext.gae.GaeAuthenticator;
 import org.restlet.routing.Router;
+import org.restlet.routing.Template;
+import org.restlet.security.ChallengeAuthenticator;
+import org.restlet.security.MapVerifier;
 
 /**
  * RESTlet Application to provide root Restlet
@@ -16,15 +21,38 @@ public class AttendanceTrackingRESTApplication extends Application {
 	 * @return Said Restlet
 	 */
 	public Restlet createInboundRoot() {
-		Router router = new Router(getContext());
+		Router baseRouter = new Router(getContext());
+		
+		// Section for Student Services, authentication through google account
+		Router studRouter = new Router(getContext());
+		studRouter.attach("/stud/attendance", AttendanceTrackingResource.class);
+		studRouter.attach("/stud/attendance/{attendanceId}", AttendanceResource.class);
+		studRouter.attach("/stud/tokens/{studentId}", AttendanceTokenResource.class);
+		studRouter.attach("/stud/info/{mail}", StudentInfoResource.class);
+		
+		GaeAuthenticator studGuard = new GaeAuthenticator(getContext());
+	    studGuard.setNext(studRouter);
 
-		router.attach("/attendance", AttendanceTrackingResource.class);
-		router.attach("/attendance/{attendanceId}", AttendanceResource.class);
-		router.attach("/tokens/{studentId}", AttendanceTokenResource.class);
-		router.attach("/post/attendance", PostMinimalAttendanceResource.class);
-		router.attach("/student/{mail}", StudentInfoResource.class);
-
-		return router;
+	    
+	    // Section for Tutor Services, hard coded authentication
+		Router tutRouter = new Router(getContext());
+		tutRouter.attach("/tut/postAttendance", PostMinimalAttendanceResource.class);
+		tutRouter.attach("/tut/attendance", AttendanceTrackingResource.class);
+		
+		// Guard the restlet with BASIC authentication.
+		ChallengeAuthenticator tutGuard = new ChallengeAuthenticator(null, ChallengeScheme.HTTP_BASIC, "testRealm");
+		// Instantiates a Verifier of identifier/secret couples based on a simple Map.
+		MapVerifier mapVerifier = new MapVerifier();
+		// Load a single static login/secret pair.
+		mapVerifier.getLocalSecrets().put("ase-tutor", "ase2017".toCharArray());
+		tutGuard.setVerifier(mapVerifier);
+		tutGuard.setNext(tutRouter);
+		
+		
+		baseRouter.attach("/stud", studGuard).setMatchingMode(Template.MODE_STARTS_WITH);
+		baseRouter.attach("/tut", tutGuard).setMatchingMode(Template.MODE_STARTS_WITH);
+		
+	    return baseRouter;
 	}
 	
 }
